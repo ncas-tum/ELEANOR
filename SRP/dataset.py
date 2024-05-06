dc747be9abc4385e1ea6f950bcec7cc039ed668d
Pedro Francisco Ortiz Costa <p.f.ortiz.costa@student.rug.nl>
2024-05-06 17:33:54 +0200

    initial commit

---
+++ SRP/dataset.py
@@ -0,0 +1,148 @@
+import pickle
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+import h5py
+
+
+def shuffle(dataset, shuffle_rng, batch_size):
+    x, y = dataset
+
+    cutoff = y.shape[0] % batch_size
+
+    obs = jax.random.permutation(shuffle_rng, x, axis=0)[:-cutoff]
+    labels = jax.random.permutation(shuffle_rng, y, axis=0)[:-cutoff]
+
+    obs = jnp.reshape(obs, (-1, batch_size) + obs.shape[1:])
+    labels = jnp.reshape(labels, (-1, batch_size))  # should make batch size a global
+
+    return (obs, labels)
+
+
+def loadNMNIST():
+    pass
+
+
+
+"""
+https://github.com/kmheckel/spyx/blob/main/research/misc/shd_evo_128c_64h.ipynb
+https://github.com/kmheckel/spyx/blob/main/spyx/fn.py
+https://github.com/kmheckel/spyx/blob/main/spyx/data.py
+1. 8000 eventos de activacion
+2. Cada evento de activacion es una lista de todas las neuronas que se activaron entre 0 y 699
+3. Cada evento de activacion es una lista de los timepos en los que se activaron aquellas neuronas
+4. Cada evento tiene su label correspondiente, que es el indice de la palabra que representa la activacion
+   La label es la version numerica de la palabra que representa
+5. La palabra textual se puede encontrar usando keys e.g. keys[label[0]] == palabra que corresponde a label en indice
+"""
+def loadShd(nb_upsample, nb_repetitions):
+    # todo: Convertir a jpn array, en vez de lista dl np arrays
+    with h5py.File('/home/pedro/neuron_pizzo_jax-main/shd_train.h5') as data:
+        train_times = data['spikes']['times'][:].tolist()
+        train_units = data['spikes']['units'][:].tolist()
+        train_labels = data['labels'][:].tolist()
+        keys = data['extra']['keys'][:].tolist()
+    
+    with h5py.File('/home/pedro/neuron_pizzo_jax-main/shd_test.h5') as data:
+        test_times = data['spikes']['times'][:].tolist()
+        test_units = data['spikes']['units'][:].tolist()
+        test_labels = data['labels'][:].tolist()
+        return train_times, train_units, train_labels, keys, test_times, test_units, test_labels
+'''
+1. Braille tiene ~4600  entries en trainset, 1080 en test set
+2. dimensiones de cada entry son uniformes, matriz de pixeles de cada
+3. 0 (espacio) (1-26) letras
+'''
+
+
+def loadBraille(nb_upsample, nb_repetitions):
+    file_name = "data_braille_letters_raw"
+    with open(file_name, "rb") as infile:
+        data_dict = pickle.load(infile)
+    
+    letter_written = [
+        "Space",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+
+    # Extract data
+    data = []
+    labels = []
+    for i, _ in enumerate(letter_written):
+        for repetition in np.arange(nb_repetitions):
+            idx = i * nb_repetitions + repetition
+            dat = 1.0 - data_dict[idx]["taxel_data"][:] / 255
+            data.append(dat)
+            labels.append(i)
+
+    # Crop to same length
+    data_steps = l = np.min([len(d) for d in data])
+    data = jnp.array([d[:l] for d in data])
+    labels = jnp.array(labels)
+
+    # Select nonzero inputs
+    nzid = [1, 2, 6, 10]
+    data = data[:, :, nzid]
+
+    # Standardize data
+    rshp = data.reshape((-1, data.shape[2]))
+    data = (data - rshp.mean(0)) / (rshp.std(0) + 1e-3)
+
+    # Upsample
+    def upsample(data, n=2):
+        shp = data.shape
+        tmp = jnp.tile(data, (1, 1, 1, n))
+        return tmp.reshape((shp[0], n * shp[1], shp[2]))
+
+    data = upsample(data, n=nb_upsample)
+
+    data = jax.random.permutation(jax.random.PRNGKey(0), data, axis=0)
+    labels = jax.random.permutation(jax.random.PRNGKey(0), labels, axis=0)
+
+    a = int(0.8 * len(labels))
+    x_train, x_test = data[:a], data[a:]
+    y_train, y_test = labels[:a], labels[a:]
+
+    nb_channels = len(nzid)
+    time_step = (
+        2e-3 / nb_upsample
+    )  # TODO needs to be updated to reflect the correct time scale
+    nb_steps = (
+        nb_upsample * data_steps
+    )  # TODO We should change this and upsample the input data
+    nb_outputs = len(np.unique(labels)) + 1
+
+    return (
+        (x_train, y_train),
+        (x_test, y_test),
+        nb_outputs,
+        nb_channels,
+        nb_steps,
+        time_step,
+    )
---
+++ SRP/network.py
@@ -0,0 +1,108 @@
+import os
+import sys
+
+import jax
+import jax.numpy as jnp
+from jax import vmap
+from spyx.axn import tanh
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pizzo_neuron_jax import FeLIF  # noqa: E402
+import time
+
+# Network definition
+def network_builder(
+    # Network params
+    # nb_inputs,
+    nb_hidden,
+    nb_outputs,
+    nb_steps,
+    # Encoding
+    # enc_fan_out,
+    alpha,
+    beta,
+    # FeLIF
+    A,
+    I_dsc,
+    V_thr,
+    P_s,
+    spike_fn=tanh(k=10),  # noqa: B008
+):
+    
+    felif_step, felif_reset = FeLIF(
+            dt=1e-3,
+            innerStep=1000,
+            A=A,
+            I_dsc=I_dsc,
+            V_thr=V_thr,
+            P_s=P_s,
+            spike_fn=spike_fn,
+            paramsScale=1e12,
+        )
+    
+    @jax.jit
+    def encoder_step(state, input_):
+        enc, input_spk = state
+
+        # Compute encoder activity
+        new_enc = (beta * enc + (1.0 - beta) * input_) * (
+            1.0 - jax.lax.stop_gradient(input_spk)
+        )
+        new_input_spk = spike_fn(enc - 1.0)
+
+        return (new_enc, new_input_spk), new_input_spk
+
+    @jax.jit
+    def hidden_step(state, input_):
+        syn, mem, out = state
+
+        # Compute hidden layer activity
+        # h1 = jnp.dot(input_, w1) #+ jnp.dot(out, v1)
+        mthr = mem - 1.0
+        new_out = spike_fn(mthr)
+        rst = jax.lax.stop_gradient(
+            new_out
+        )  # We do not want to backprop through the reset
+
+        new_syn = alpha * syn + input_
+        new_mem = (beta * mem + (1.0 - beta) * syn) * (1.0 - rst)
+
+        return (new_syn, new_mem, new_out), new_out
+
+    @jax.jit
+    def output_step(state, input_):
+        syn, mem = state
+
+        new_syn = alpha * syn + input_
+        new_mem = beta * mem + (1.0 - beta) * syn
+        return (new_syn, new_mem), new_mem
+
+    @jax.jit
+    def predict(params, input_):
+        h1 = jnp.dot(input_, params[0])
+        syn = jnp.zeros((nb_hidden,))
+        mem = jnp.zeros((nb_hidden,))
+        out = jnp.zeros((nb_hidden,))
+        _, spk_rec = jax.lax.scan(hidden_step, (syn, mem, out), h1, nb_steps, unroll=1)
+
+        h2 = jnp.dot(spk_rec, params[1])
+        syn2 = jnp.zeros((nb_outputs,))
+        mem2 = jnp.zeros((nb_outputs,))
+        # _, out_rec = jax.lax.scan(output_step, (syn2, mem2), h2, nb_steps, unroll=1)
+        #return out_rec, h2, spk_rec, h1
+        _, (out_rec, charge, V_rec, P_Rec) = jax.lax.scan(
+            felif_step,
+            felif_reset(nb_outputs),
+            h2,
+            nb_steps,
+            unroll=1,
+        )
+        # print(out_rec_felif.shape)
+        # print(out_rec.shape)
+    
+        return out_rec, charge, V_rec, P_Rec, h2, spk_rec, h1
+
+    batched_predict = vmap(predict, in_axes=(None, 0))
+
+    return batched_predict
