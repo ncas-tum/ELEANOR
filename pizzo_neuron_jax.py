0d9791f841cc152709ce3357b3139945ca50eaa4
F.M. Quintana Velazquez <f.m.quintana.velazquez@rug.nl>
2024-07-19 18:24:33 +0200

    Update base FeLIF model with new method

--- pizzo_neuron_jax.py
+++ pizzo_neuron_jax.py
@@ -1,145 +1,154 @@
+from typing import Any, Callable
+
+import equinox as eqx
 import jax
 import jax.numpy as jnp
-from spyx.axn import custom, superspike
+from spyx.axn import arctan
+
+_spike_fn = arctan(k=2)
 
 
-def FeLIF(
-    A=25e-12,  # device area
-    t_hzo=10e-9,  # thikness ferroelectric
-    t_int=1.375e-9,  # thikness interlayer
-    eps_hzo=25.2,  # ferroelectric dielectric constant
-    eps_int=33,  # interlayer dielectric constant
-    E_a=12.7e8,  # coercitive field
-    P_s=22e-2,  # max polarisation
-    tau_0=1e-13,  # multiplicative factor for switching time constant
-    I_0=1e-4,  # mult factor for leakage current
-    V_t=0.32,  # normalization factor for voltage in leakage current
-    C_par=15e-15,  # parasitic capacitance form the circuit
-    alpha=1.3,  # to fit tau exponential
-    soft_E=5e6,  # soft boudary for the electric field, avoid tau to diverge
-    I_dsc=10e-12,  # discharge current, set the "dendritic time constant"
-    V_thr=2.5,
-    dt=1e-3,  # 1us timestep resolution
-    innerStep=1000,
-    paramsScale=1e12,  # Scale parameters to avoid underflow
-    spike_fn=None,
-):
-    A = A * paramsScale  # device area
-    t_hzo = t_hzo * paramsScale  # thikness ferroelectric
-    t_int = t_int * paramsScale  # thikness interlayer
-    E_a = E_a / paramsScale  # coercitive field
-    C_par = C_par * paramsScale  # parasitic capacitance form the circuit
-    soft_E = (
-        soft_E / paramsScale
-    )  # soft boudary for the electric field, avoid tau to diverge
-    I_dsc = I_dsc * paramsScale  # discharge current, set the "dendritic time constant"
+class FeLIF(eqx.Module):
+    out_size: int = eqx.field(static=True)
+    P_s: float = eqx.field(static=True)  # max polarisation
+    A: float = eqx.field(static=True)
+    C_tot: float = eqx.field(static=True)
+    step: Callable[[Any, Any], Any] = eqx.field(static=True)
 
-    _eps0 = 8.85418792394420013968e-12 * paramsScale
+    def __init__(
+        self,
+        out_size,
+        A=25e-12,  # device area
+        t_hzo=10e-9,  # thikness ferroelectric
+        t_int=1.375e-9,  # thikness interlayer
+        eps_hzo=25.2,  # ferroelectric dielectric constant
+        eps_int=33,  # interlayer dielectric constant
+        E_a=12.7e8,  # coercitive field
+        P_s=22e-2,  # max polarisation
+        tau_0=1e-13,  # multiplicative factor for switching time constant
+        I_0=1e-4,  # mult factor for leakage current
+        V_t=0.32,  # normalization factor for voltage in leakage current
+        C_par=15e-15,  # parasitic capacitance form the circuit
+        alpha=1.3,  # to fit tau exponential
+        soft_E=5e6,  # soft boudary for the electric field, avoid tau to diverge
+        I_dsc=10e-12,  # discharge current, set the "dendritic time constant"
+        V_thr=2.5,
+        dt=1e-3,  # 1us timestep resolution
+        paramsScale=1e12,  # Scale parameters to avoid underflow
+        spike_fn=_spike_fn,
+        *,
+        key=None
+    ):
+        _eps0 = 8.85418792394420013968e-12 * paramsScale
+        self.out_size = out_size
 
-    C_0 = _eps0 * eps_hzo / t_hzo * A
-    C_tot = C_0 + C_par
-    cap_divider = eps_int / (t_hzo * eps_hzo + t_int * eps_int)
-    depol_divider = 1 / _eps0 * t_int / (t_hzo * eps_hzo + t_int * eps_int)
+        # Parameters
+        A = A * paramsScale
+        t_hzo = t_hzo * paramsScale
+        t_int = t_int * paramsScale
+        E_a = E_a / paramsScale
+        C_par = C_par * paramsScale
+        soft_E = soft_E / paramsScale
+        I_dsc = I_dsc * paramsScale
 
-    if spike_fn is None:
-        spike_fn = superspike()
+        C_0 = _eps0 * eps_hzo / t_hzo * A
+        C_tot = C_0 + C_par
 
-    _tau_fn = custom(
-        lambda x: 1 / x,
-        lambda x: tau_0 * jnp.exp((E_a / (jnp.abs(x) + soft_E)) ** alpha),
-    )
+        cap_divider = eps_int / (t_hzo * eps_int + t_int * eps_hzo)
+        depol_divider = 1 / _eps0 * t_int / (t_hzo * eps_int + t_int * eps_hzo)
 
-    def _derivative2(v, p):
-        E = v * cap_divider - p * depol_divider
-        tau = _tau_fn(E)
+        # Save parameters on object
+        self.A = A
+        self.P_s = P_s
+        self.C_tot = C_tot
 
-        I_leak = I_0 * A * jnp.expm1(v / V_t) + I_dsc * jnp.sign(v)
-        I_p = (jnp.sign(E) * P_s - p) * A / jax.lax.stop_gradient(tau)
+        @jax.custom_vjp
+        def tau_fn(E):
+            tau = tau_0 * jnp.exp((E_a / (jnp.abs(E) + 5e-6)) ** alpha)
 
-        dv = (1 / C_tot) * (-I_leak - I_p)
-        dp = I_p / A
+            return tau
 
-        return (dv, dp)
+        def tau_fn_fwd(E):
+            return tau_fn(E), (E,)
 
-    def _derivative(state, input_):
-        (v, p) = state
+        def tau_fn_bw(res, g):
+            (E,) = res
+            exp_x = jnp.clip((E_a / (jnp.abs(E) + soft_E)) ** alpha, 0, 1)
+            tau_prime = -(
+                tau_0
+                * E_a
+                * alpha
+                * E
+                * jnp.exp(exp_x)
+                * (E_a / (jnp.abs(E) + soft_E)) ** (alpha - 1)
+            ) / (jnp.abs(E) * (E + soft_E) ** 2)
 
-        E = v * cap_divider - p * depol_divider
-        # tau = tau_0 * _exp_fn(
-        #     (E_a / (jnp.abs(E) + soft_E)) ** alpha
-        # )
-        tau = _tau_fn(E)
-        tau = jnp.clip(tau, 1.1e-7, 1.1e7)
+            tangents_out = (g * tau_prime,)
+            return tangents_out
 
-        I_leak = (I_0 * A * jnp.expm1(v / V_t) + I_dsc) * jnp.sign(v)
-        I_p = (jnp.sign(E) * P_s - p) * A / tau
+        tau_fn.defvjp(tau_fn_fwd, tau_fn_bw)
 
-        # Limit the currents to 100uA
-        I_p = jnp.clip(I_p, -100e-6 * paramsScale, 100e-6 * paramsScale)
-        I_leak = jnp.clip(I_leak, -100e-6 * paramsScale, 100e-6 * paramsScale)
+        def updatePol(v, p):
 
-        dv = (1 / C_tot) * (input_ - I_leak - I_p)
-        dp = I_p / A
+            def pol_step(state, xs):
+                p, _ = state
+                E = v * cap_divider - p * depol_divider
 
-        return dv, dp
+                tau = tau_fn(E)
 
-    @jax.jit
-    def inner_loop(state, input_):
-        (v, p, s) = state
+                I_p_new = (jnp.sign(E) * P_s - p) * A / jax.lax.stop_gradient(tau)
+                dp = I_p_new / A
+                p = jnp.clip(p + 1e-3 * dt * dp, -P_s, P_s)
+                return (p, I_p_new), None
 
-        (dv, dp) = _derivative((v, p), input_)
+            init_state = (p, jnp.zeros_like(p))
+            (p_inner, I_p_inner), _ = jax.lax.scan(
+                pol_step, init_state, xs=None, length=1000
+            )
 
-        v_new = v + (1 - s) * dt / innerStep * dv
-        p_new = p + (1 - s) * dt / innerStep * dp
+            (p_outer, I_p_outer), _ = pol_step(init_state, None)
 
-        v_new = jnp.clip(v_new, 0, 5)
-        p_new = jnp.clip(p_new, -P_s, P_s)
+            p = p_outer + jax.lax.stop_gradient(p_inner - p_outer)
+            I_p = I_p_outer + jax.lax.stop_gradient(I_p_inner - I_p_outer)
 
-        s_new = spike_fn(v_new - V_thr)
+            return p, I_p
 
-        return (v_new, p_new, s_new), None
+        def step(state, input_):
+            v, p = state
 
-    def step(state, input_):
-        v, p = state
+            spikes = spike_fn(v - V_thr)
 
-        # Inner loop with 10us pulse input current
-        last_state, _ = jax.lax.scan(
-            inner_loop,
-            (v, p, jnp.zeros_like(v)),
-            jnp.stack(
-                [input_ * 1e5 * C_tot] * 10  # Convert the input into current
-                + [jnp.zeros_like(input_)] * (innerStep - 10)  # 10 us current
-            ),
-            innerStep,
-            unroll=1,
-        )
-        (v_inner, p_inner, _) = last_state
+            # Update polarization
+            p_upper, I_p = updatePol(v, p)
 
-        dv, dp = _derivative2(v, p)
+            # Update voltage
+            I_leak = (I_0 * A * jnp.expm1(v / V_t) + I_dsc) * jnp.sign(v)
+            dv = (input_ - I_leak - jax.lax.stop_gradient(I_p)) / C_tot
+            v_upper = jnp.clip(v + dt * dv, 0, 5)
 
-        v_upper = v + dt * dv + input_
-        p_upper = p + dt * dp
+            # Reset when spike
+            spikes_ref = jax.lax.stop_gradient(spikes)
+            v_new = (1 - spikes_ref) * v_upper
+            p_new = (1 - spikes_ref) * p_upper - (spikes_ref * P_s)
 
-        # Limit voltage and polarization
-        v_upper = jnp.clip(v_upper, 0, 5)
-        p_upper = jnp.clip(p_upper, -P_s, P_s)
+            return (v_new, p_new), (v_new, p_new, spikes)
 
-        v_ste = v_upper + jax.lax.stop_gradient(v_inner - v_upper)
-        p_ste = p_upper + jax.lax.stop_gradient(p_inner - p_upper)
+        self.step = jax.jit(step)
 
-        spikes = spike_fn(v_ste - V_thr)
-        v_new = (1 - spikes) * v_ste
-        p_new = (1 - spikes) * p_ste - (spikes * P_s)
+    @eqx.filter_jit
+    def getCharge(self, v, p):
+        return v * self.C_tot + p * self.A
 
-        return (v_new, p_new), (spikes, v_new * C_tot + p_new * A, v_new, p_new)
+    @jax.named_scope("nn.FeLIF")
+    def __call__(self, input_):
+        v0 = jnp.zeros((self.out_size,))
+        p0 = jnp.zeros((self.out_size,)) - self.P_s
 
-    def initial_state(nb_neurons):
-        V0 = jnp.zeros((nb_neurons,))
-        P0 = jnp.zeros((nb_neurons,)) - P_s
+        state = (v0, p0)
+        _, out = jax.lax.scan(self.step, state, input_)
+        v, p, s = out
 
-        return (V0, P0)
-
-    return jax.jit(step), initial_state
+        return s, self.getCharge(v, p), v, p
 
 
 if __name__ == "__main__":
@@ -150,56 +159,72 @@ if __name__ == "__main__":
     plt.style.use("dark_background")
 
     # Input every 5ms
-    input_ = jnp.array([1.0, 0, 0, 0, 0] * 20)
-    target = jnp.array([5.0])
-    felif_step, felif_reset = FeLIF(
-        dt=1e-3,
-        innerStep=1000,
-        A=25e-12,
-        I_dsc=3.2532312693054174e-11,
-        V_thr=1.698682296181096,
-        P_s=0.13321217250476625,
-        spike_fn=superspike(),
-        paramsScale=1e12,
-    )
+    input_ = 500 * jnp.array([[1.0], [0], [0], [0], [0], [0], [0], [0], [0], [0]] * 10)
+    target = jnp.array([2.0])
 
-    @jax.jit
-    def predict(params, input_):
-        _, (spikes, charge, V, P) = jax.lax.scan(
-            felif_step, felif_reset(1), input_ * params[0], input_.shape[0], unroll=20
-        )
+    class Network(eqx.Module):
+        linear: eqx.nn.Linear
+        felif: FeLIF
+
+        def __init__(self, *, key):
+            linear = eqx.nn.Linear(1, 1, use_bias=False, key=key)
+            self.felif = FeLIF(
+                1,
+                dt=1e-3,
+                A=25e-12,
+                I_dsc=3.2532312693054174e-11,
+                V_thr=1.698682296181096,
+                P_s=0.13321217250476625,
+                spike_fn=arctan(),
+                paramsScale=1e12,
+            )
+            new_weight = jnp.asarray([[1.0]])
+            self.linear = eqx.tree_at(lambda x: x.weight, linear, new_weight)
+
+        @jax.named_scope("nn.Network")
+        def __call__(self, input_):
+            x1 = jax.vmap(self.linear)(input_)
+            s, c, v, p = self.felif(x1)
+
+            return s, c, v, p
+
+    @eqx.filter_jit
+    def predict(model, input_):
+        spikes, charge, V, P = model(input_)
+
         return spikes, charge, V, P
 
-    @jax.jit
-    def loss(params, input_, target):
-        preds, _, _, _ = predict(params, input_)
+    @eqx.filter_jit
+    def loss(model, input_, target):
+        preds, _, _, _ = predict(model, input_)
         preds = jnp.sum(preds, axis=0)  # Sum over time
         return jnp.mean((preds - target) ** 2)
 
-    params = [jnp.ones((1,)) * 0.3]
-    opt = optax.sgd(learning_rate=1e-2)
-    opt_state = opt.init(params)
+    model = Network(key=jax.random.key(0))
+    opt = optax.sgd(learning_rate=1e-3)
+    opt_state = opt.init(model)
 
-    nb_epochs = 500
+    nb_epochs = 10
 
     print("Optimizing")
     loss_rec = []
     param_rec = []
-    state = [params, opt_state]
+    state = [model, opt_state]
     for _ in trange(nb_epochs):
         grad_params, opt_state = state
 
-        loss_val, grads = jax.value_and_grad(loss)(grad_params, input_, target)
+        loss_val, grads = eqx.filter_value_and_grad(loss)(grad_params, input_, target)
         updates, opt_state = opt.update(grads, opt_state, grad_params)
         loss_rec.append(loss_val)
 
-        new_param = optax.apply_updates(grad_params, updates)
+        new_param = eqx.apply_updates(grad_params, updates)
         state = [new_param, opt_state]
-        param_rec.append(new_param[0])
+        param_rec.append(new_param.linear.weight[0, 0])
     loss_rec = jnp.stack(loss_rec)
     param_rec = jnp.stack(param_rec)
+
     print("Predict")
-    _, preds_before, _, _ = predict(params, input_)
+    _, preds_before, _, _ = predict(model, input_)
     _, preds, V, P = predict(state[0], input_)
     print("Finish")
 
