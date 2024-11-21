4772d9123c5bb0bc8c5c436a04a19eccd2c24227
Fernando M. Quintana Velázquez <f.m.quintana.velazquez@rug.nl>
2024-11-21 16:56:40 +0100

    Updated optuna experiment for launching multiple simulations

--- scripts/benchmark/optuna/experiment.py
+++ scripts/benchmark/optuna/experiment.py
@@ -1,5 +1,5 @@
-import pickle
-from typing import Optional
+import argparse
+from typing import Union, Callable, Optional, Sequence
 from functools import partial
 
 import jax
@@ -12,8 +12,10 @@ import jax.random as jrandom
 import optuna.storages.journal
 from chex import Array, PRNGKey
 from tqdm import trange
+from snnax.snn.layers.stateful import StateShape, StatefulOutput, default_init_fn
+from snnax.functional.surrogate import SpikeFn, superspike_surrogate
 
-from eleanor.models import FeLIFV2
+from eleanor.models import FeLIFV2, Heracles
 from eleanor.datasets import shuffle, loadBraille
 from eleanor.weight_quantization import QuantizedLinear
 
@@ -38,38 +40,145 @@ class EncodingLayer(eqx.Module):
         return output
 
 
-def define_model(key, trial):
+class RLIF(snn.LIF):
+    recurrent: eqx.Module
+
+    def __init__(
+        self,
+        n_features,
+        quant_bits,
+        decay_constants: Union[Sequence[float], Array],
+        spike_fn: SpikeFn = superspike_surrogate(10.0),
+        threshold: Array = 1.0,
+        stop_reset_grad: bool = True,
+        reset_val: Optional[Array] = None,
+        init_fn: Optional[Callable] = default_init_fn,
+        shape: Optional[StateShape] = None,
+        key: Optional[PRNGKey] = None,
+    ) -> None:
+
+        key, krec = jrandom.split(key, 2)
+        super().__init__(
+            decay_constants,
+            spike_fn,
+            threshold,
+            stop_reset_grad,
+            reset_val,
+            init_fn,
+            shape,
+            key,
+        )
+        if quant_bits == "FP":
+            self.recurrent = eqx.nn.Linear(
+                n_features, n_features, use_bias=False, key=krec
+            )
+        else:
+            quant_bits = int(quant_bits)
+            self.recurrent = QuantizedLinear(
+                n_features, n_features, quant_bits=quant_bits, use_bias=False, key=krec
+            )
+
+    def __call__(
+        self,
+        state: Sequence[Array],
+        synaptic_input: Array,
+        *,
+        key: Optional[PRNGKey] = None,
+    ) -> StatefulOutput:
+        mem_pot, syn_curr, spike_output = state
+
+        if self.reset_val is None:
+            reset_pot = mem_pot * spike_output
+        else:
+            reset_pot = (mem_pot - self.reset_val) * spike_output
+
+        # Optionally stop gradient propagation through refectory potential
+        refectory_potential = (
+            jax.lax.stop_gradient(reset_pot) if self.stop_reset_grad else reset_pot
+        )
+        mem_pot = mem_pot - refectory_potential
+
+        alpha = jax.lax.clamp(0.5, self.decay_constants[0], 1.0)
+        beta = jax.lax.clamp(0.5, self.decay_constants[1], 1.0)
+
+        mem_pot = alpha * mem_pot + (1.0 - alpha) * (
+            syn_curr + self.recurrent(spike_output, key=key)
+        )
+        syn_curr = beta * syn_curr + (1.0 - beta) * synaptic_input
+
+        spike_output = self.spike_fn(mem_pot - self.threshold)
+
+        state = [mem_pot, syn_curr, spike_output]
+        return [state, spike_output]
+
+
+def define_model(key, model_name, quant_bits, trial):
     # encoding_gain = trial.suggest_float("encoding_gain", 0.01, 1.0, log=False)
     alpha = trial.suggest_float("alpha", 0.1, 1.0, log=False)
     beta = trial.suggest_float("beta", 0.1, 1.0, log=False)
-    # alpha_o = trial.suggest_float("alpha_o", 0.1, 1.0, log=False)
-    # beta_o = trial.suggest_float("beta_o", 0.1, 1.0, log=False)
-    # scaler = trial.suggest_float("scaler", 1.0, 1000.0, log=False)
-    V_thr = trial.suggest_float("V_thr", 2.5, 3.5, log=False)
-    paramScale = 10 ** trial.suggest_int("paramScale", 5, 12)
 
-    key1, key2, key3, key4, key5, key6 = jrandom.split(key, 6)
+    key1, key2, key3, key4 = jrandom.split(key, 4)
+    if model_name == "LIF" or model_name == "RLIF":
+        alpha_o = trial.suggest_float("alpha_o", 0.1, 1.0, log=False)
+        beta_o = trial.suggest_float("beta_o", 0.1, 1.0, log=False)
+        ouputLayer = snn.LIF([alpha_o, beta_o], key=key4)
+    elif model_name == "Heracles" or model_name == "FeLIF":
+        V_thr = trial.suggest_float("V_thr", 2.5, 3.5, log=False)
+        paramScale = 10 ** trial.suggest_int("paramScale", 5, 12)
+        if model_name == "FeLIF":
+            ouputLayer = FeLIFV2(dt=1e-3, V_thr=V_thr, paramsScale=paramScale, key=key4)
+        else:
+            ouputLayer = Heracles(
+                dt=1e-3, V_thr=V_thr, paramsScale=paramScale, key=key4
+            )
+    else:
+        raise Exception(f"Model {model_name} not found")
+
+    if model_name == "RLIF":
+        hiddenLayer = RLIF(256, quant_bits, [alpha, beta], key=key2)
+    else:
+        hiddenLayer = snn.LIF([alpha, beta], key=key2)
+
     enc_gain = jax.random.normal(key1, shape=(128,)) * 0.18436009935019085
     enc_bias = jax.random.normal(key2, shape=(128,))
-    model = snn.Sequential(
-        EncodingLayer(enc_gain, enc_bias, 32),
-        QuantizedLinear(128, 256, quant_bits=3, key=key3),
-        snn.LIF([alpha, beta], key=key4),
-        QuantizedLinear(256, 27, quant_bits=3, key=key5),
-        # snn.LIF([alpha_o, beta_o], key=key6),
-        FeLIFV2(dt=1e-3, V_thr=V_thr, paramsScale=paramScale, key=key6),
-        # Heracles(dt=1e-3, V_thr=V_thr, paramsScale=paramScale, key=key6),
-    )
+
+    if quant_bits == "FP":
+        model = snn.Sequential(
+            EncodingLayer(enc_gain, enc_bias, 32),
+            eqx.nn.Linear(128, 256, use_bias=False, key=key1),
+            hiddenLayer,
+            eqx.nn.Linear(256, 27, use_bias=False, key=key3),
+            ouputLayer,
+        )
+    else:
+        quant_bits = int(quant_bits)
+        model = snn.Sequential(
+            EncodingLayer(enc_gain, enc_bias, 32),
+            QuantizedLinear(128, 256, use_bias=False, quant_bits=quant_bits, key=key1),
+            hiddenLayer,
+            QuantizedLinear(256, 27, use_bias=False, quant_bits=quant_bits, key=key3),
+            ouputLayer,
+        )
     return model
+
+
+def _outputLIF(model, in_states, in_spikes, key):
+    out_state, out_spikes = model(in_states, in_spikes, key=key)
+    final_layer_out = out_spikes[-1]
+    return final_layer_out
+
+
+def _outputFeLIF(model, in_states, in_spikes, key):
+    out_state, out_spikes = model(in_states, in_spikes, key=key)
+    final_layer_out = out_spikes[-1][0]
+    return final_layer_out
 
 
 # Simple batched loss function
 @partial(jax.vmap, in_axes=(None, None, 0, 0, 0))
 def loss_fn(model, in_states, in_spikes, tgt_class, key):
-    out_state, out_spikes = model(in_states, in_spikes, key=key)
-
     # Get the output of last layer
-    final_layer_out = out_spikes[-1][0]
+    final_layer_out = getOutput(model, in_states, in_spikes, key)
     # final_layer_out = out_spikes[-1]
     pred = final_layer_out.sum(axis=0)
 
@@ -88,9 +197,7 @@ def loss_and_grad(model, in_states, in_spikes, tgt_class, key):
 
 @partial(jax.vmap, in_axes=(None, None, 0, 0, 0))
 def accuracy_fn(model, in_states, in_spikes, tgt_class, key):
-    out_state, out_spikes = model(in_states, in_spikes, key=key)
-    final_layer_out = out_spikes[-1][0]
-    # final_layer_out = out_spikes[-1]
+    final_layer_out = getOutput(model, in_states, in_spikes, key)
     pred = final_layer_out.sum(axis=0)
     predicted_class = jnp.argmax(pred)
     return predicted_class == tgt_class
@@ -117,7 +224,7 @@ def update(model, optim, in_states, opt_state, in_spikes, tgt_class, key):
     return model, opt_state, loss
 
 
-def objective(trial):
+def _objective(model_name, quant_bits, trial):
     trainset, testset, nb_outputs, nb_channels, nb_steps, time_step = loadBraille(
         2, 200
     )
@@ -125,7 +232,8 @@ def objective(trial):
     key = jrandom.key(SEED)
     key, kmodel, kstate = jrandom.split(key, 3)
 
-    model = define_model(kmodel, trial)
+    model = define_model(kmodel, model_name, quant_bits, trial)
+    print(model)
     optim = optax.adamax(
         learning_rate=trial.suggest_float("lr", 1e-5, 1e-1, log=True), b1=0.9, b2=0.995
     )
@@ -166,43 +274,26 @@ def objective(trial):
     return accuracy_test
 
 
-class SaveStateCallback:
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--model", type=str, required=True)
+parser.add_argument("-q", "--quantization", type=str, required=True)
 
-    def __call__(
-        self, study: optuna.study.Study, trial: optuna.trial.FrozenTrial
-    ) -> None:
-        with open(f"sampler_{quantization}bit_{model}.pkl", "wb") as fout:
-            pickle.dump(study.sampler, fout)
-        with open(f"pruner_{quantization}bit_{model}.pkl", "wb") as fout:
-            pickle.dump(study.pruner, fout)
+args = parser.parse_args()
+print(args.model, args.quantization)
+print("60 Trials")
 
+if args.model == "LIF" or args.model == "RLIF":
+    getOutput = _outputLIF
+elif args.model == "Heracles" or args.model == "FeLIF":
+    getOutput = _outputFeLIF
+else:
+    raise Exception(f"Model {args.model} not found")
 
-model = "FeLIF"
-quantization = "3"
-storage = optuna.storages.RDBStorage("sqlite:///bruno.db")
-
-# try:
-#     restored_sampler = pickle.load(open(f"sampler_{quantization}bit_{model}.pkl", "rb"))
-# except FileNotFoundError:
-#     restored_sampler = optuna.samplers.TPESampler(seed=SEED)
-
-# try:
-#     restored_pruner = pickle.load(open(f"pruner_{quantization}bit_{model}.pkl", "rb"))
-# except FileNotFoundError:
-#     restored_pruner = optuna.pruners.NopPruner()
-
-# study = optuna.create_study(
-#     storage=storage,
-#     study_name=f"{quantization}bit {model}",
-#     direction="maximize",
-#     sampler=restored_sampler,
-#     pruner=restored_pruner,
-#     load_if_exists=True,
-# )
+objective = partial(_objective, args.model, args.quantization)
+storage = optuna.storages.RDBStorage("sqlite:///bruno_results.db")
 
 study = optuna.load_study(
     storage=storage,
-    study_name=f"{quantization}bit {model}",
+    study_name=f"{args.quantization}bit {args.model}",
 )
-# study.optimize(objective, n_trials=60, callbacks=[SaveStateCallback()])
 study.optimize(objective, n_trials=60)
